92efc8344808a52abd27862261ec213d6841a067
Jonathan Pierce <jonathanpierce@JPs-MacBook-Pro.local>
2025-04-30 15:28:20 -0600

    first commit

---
+++ Policies.ps1
@@ -0,0 +1,22 @@
+Import-Module BitsTransfer
+
+#Transfer BitLocker Task
+Start-BitsTransfer -Source  -Destination C:\Policies\BL_Enable_Store.ps1
+
+#Create Action to be run by task
+$action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument '-ExecutionPolicy bypass -File C:\Policies\BL_Enable_Store.ps1'
+
+#Create trigger for task
+$trigger = New-ScheduledTaskTrigger -AtLogon
+
+#Create task
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "BitLocker" -User "NT AUTHORITY\SYSTEM"
+
+#Unregister old task
+Unregister-ScheduledTask -TaskName "BL_Enable-Backup" -Confirm:$false
+
+#Transfer GPO Reg
+Start-BitsTransfer -Source  -Destination C:\Policies\GPO.reg
+reg import C:\Policies\GPO.reg
+
+New-Item -ItemType file -Path C:\Policies -Name "Policies Applied $(Get-Date -Format 'yyyyMMdd_HHmmss').log"
