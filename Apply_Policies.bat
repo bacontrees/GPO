1e9f1010d680ecbf3750f7755f67f06c9ff2602d
JP <pierce.jonathan@gmail.com>
2025-04-30 15:51:59 -0600

    Commit

--- Apply_Policies.bat
+++ Apply_Policies.bat
@@ -1,2 +1,3 @@
+if not exist "C:\Policies'" mkdir C:\Policies
 bitsadmin /transfer Policies https://raw.githubusercontent.com/bacontrees/AcademyAAD/refs/heads/main/Policies.ps1 C:\Policies\Policies.ps1
 powershell.exe -ExecutionPolicy bypass -File C:\Policies\Policies.ps1
