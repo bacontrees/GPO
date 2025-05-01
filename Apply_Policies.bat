f0e7f49abc51c4127cd4f66b0ddce520ba6169f4
bacontrees <45217496+bacontrees@users.noreply.github.com>
2025-05-01 05:53:56 -0600

    Update Apply_Policies.bat

--- Apply_Policies.bat
+++ Apply_Policies.bat
@@ -1,3 +1,6 @@
 if not exist "C:\Policies'" mkdir C:\Policies
 bitsadmin /transfer Policies https://raw.githubusercontent.com/bacontrees/AcademyAAD/refs/heads/main/Policies.ps1 C:\Policies\Policies.ps1
 powershell.exe -ExecutionPolicy bypass -File C:\Policies\Policies.ps1
+
+bitsadmin /transfer printbrm https://download.canyoncs.com/printbrm.printerExport c:\Policies\printbrm.printerExport
+%Windir%\System32\spool\tools\printbrm.exe -r -noACL -f c:\Policies\printbrm.printerExport -o force
