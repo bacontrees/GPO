abdc923e2b07b7df8ff92bac54cabd8b2820f6c2
bacontrees <45217496+bacontrees@users.noreply.github.com>
2025-05-01 06:03:34 -0600

    Add files via upload

---
+++ Update_Printers_All.bat
@@ -0,0 +1,2 @@
+bitsadmin /transfer printbrm https://download.canyoncs.com/printbrm.printerExport c:\Policies\printbrm.printerExport
+%Windir%\System32\spool\tools\printbrm.exe -r -noACL -f c:\Policies\printbrm.printerExport -o force
