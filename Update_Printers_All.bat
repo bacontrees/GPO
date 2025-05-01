dda8003a8b743a97f685c3bb45344843be43f929
bacontrees <45217496+bacontrees@users.noreply.github.com>
2025-05-01 06:04:11 -0600

    Update Update_Printers_All.bat

--- Update_Printers_All.bat
+++ Update_Printers_All.bat
@@ -1,2 +1,3 @@
+if not exist "C:\Policies\" mkdir C:\Policies
 bitsadmin /transfer printbrm https://download.canyoncs.com/printbrm.printerExport c:\Policies\printbrm.printerExport
 %Windir%\System32\spool\tools\printbrm.exe -r -noACL -f c:\Policies\printbrm.printerExport -o force
