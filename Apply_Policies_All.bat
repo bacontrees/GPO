fbb595c1e5b4c5c2dcd9a9332cf5a1b79a33a70e
bacontrees <45217496+bacontrees@users.noreply.github.com>
2025-05-28 09:17:30 -0600

    Update Apply_Policies_All.bat

--- Apply_Policies_All.bat
+++ Apply_Policies_All.bat
@@ -1,5 +1,5 @@
 if not exist "C:\Policies\" mkdir C:\Policies
-bitsadmin /transfer Policies https://raw.githubusercontent.com/bacontrees/AcademyAAD/refs/heads/main/Policies.ps1 C:\Policies\Policies.ps1
+bitsadmin /transfer Policies https://raw.githubusercontent.com/bacontrees/GPO/refs/heads/main/Policies.ps1 C:\Policies\Policies.ps1
 powershell.exe -ExecutionPolicy bypass -File C:\Policies\Policies.ps1
 
 if not exist "C:\Policies\" mkdir C:\Policies
