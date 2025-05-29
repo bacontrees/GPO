3dc682a79fc2141fcad5eccf9f8d95053ab34931
bacontrees <45217496+bacontrees@users.noreply.github.com>
2025-05-29 08:43:04 -0600

    Update TAB_Apply_Policies_All.bat

--- TAB_Apply_Policies_All.bat
+++ TAB_Apply_Policies_All.bat
@@ -1,6 +1,6 @@
 if not exist "C:\Policies\" mkdir C:\Policies
-bitsadmin /transfer Policies https://raw.githubusercontent.com/bacontrees/GPO/refs/heads/main/Policies.ps1 C:\Policies\Policies.ps1
-powershell.exe -ExecutionPolicy bypass -File C:\Policies\Policies.ps1
+bitsadmin /transfer Policies https://raw.githubusercontent.com/bacontrees/GPO/refs/heads/main/Policies_TAB.ps1 C:\Policies\Policies_TAB.ps1
+powershell.exe -ExecutionPolicy bypass -File C:\Policies\Policies_TAB.ps1
 
 if not exist "C:\Policies\" mkdir C:\Policies
 bitsadmin /transfer printbrm https://download.canyoncs.com/printbrm.printerExport c:\Policies\printbrm.printerExport
