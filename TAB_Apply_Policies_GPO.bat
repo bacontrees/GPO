4d3ad9d3a9e71106000e6d81862be8497a11e8e1
bacontrees <45217496+bacontrees@users.noreply.github.com>
2025-05-29 08:44:44 -0600

    Update TAB_Apply_Policies_GPO.bat

--- TAB_Apply_Policies_GPO.bat
+++ TAB_Apply_Policies_GPO.bat
@@ -1,3 +1,3 @@
 if not exist "C:\Policies\" mkdir C:\Policies
 bitsadmin /transfer Policies https://raw.githubusercontent.com/bacontrees/GPO/refs/heads/main/Policies_TAB.ps1 C:\Policies\Policies_TAB.ps1
-powershell.exe -ExecutionPolicy bypass -File C:\Policies\Policies.ps1
+powershell.exe -ExecutionPolicy bypass -File C:\Policies\Policies_TAB.ps1
